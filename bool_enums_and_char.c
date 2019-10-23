0dc08f65e43bcc02e01c650e0644ef9276beb78a
Henrique de Moraes <56733528+hmoraes47@users.noreply.github.com>
2019-10-23 11:50:30 -0300

    Create bool_enums_and_char.c

---
+++ bool_enums_and_char.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+/* Bool */
+int main(void)
+{
+  _bool boolvar = 0; /* Or 1 */
+  
+
+/* Enums */
+  enum primaryColor { red, yellow, blue };
+
+
+/* Char */
+  char broiled; /* Declare a char variable */
+  broiled = "T"; /* OK */
+}
