e8a8453abd4075fd707d480c28f772cbb0283fdd
Henrique de Moraes <56733528+hmoraes47@users.noreply.github.com>
2019-10-24 15:05:12 -0300

    Update bitwise.c

--- bitwise.c
+++ bitwise.c
@@ -14,3 +14,83 @@ int main()
 
    return 0;
 }
+
+/* WITH THE OPERATOR | :
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   unsigned a = 60; // 0011 1100
+   unsigned b = 13; // 0000 1101
+
+   int result = 0;
+
+   result = a | b; // result = 0011 1101 (= 61)
+
+   printf("Result is: %d", result);
+
+   return 0;
+}
+
+*/
+
+/* WITH THE OPERATOR ^ :
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   unsigned a = 60; // 0011 1100
+   unsigned b = 13; // 0000 1101
+
+   int result = 0;
+
+   result = a ^ b; // result = 0011 0001 (= 49)
+
+   printf("Result is: %d", result);
+
+   return 0;
+}
+
+*/
+
+/* WITH THE OPERATOR ~ :
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   unsigned a = 60; // 0011 1100
+   unsigned b = 13; // 0000 1101
+
+   int result = 0;
+
+   result = ~a;
+
+   printf("Result is: %d", result);
+
+   return 0;
+}
+
+*/
+
+/* WITH THE OPERATOR << or >> :
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   unsigned a = 60; // 0011 1100
+   unsigned b = 13; // 0000 1101
+
+   int result = 0;
+
+   result = a << 2; // 1111 0000 (= 240)
+
+   printf("Result is: %d", result);
+
+   return 0;
+}
+
+*/
