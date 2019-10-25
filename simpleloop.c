f6957931df4459b558bc168742cd7db6347777ce
Henrique de Moraes <56733528+hmoraes47@users.noreply.github.com>
2019-10-25 08:46:59 -0300

    Create simpleloop.c

---
+++ simpleloop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int score = 95;
+int big = 90
+
+//Simple statement, no brackets
+if(score > big)
+{
+  printf("Jackpot!!!\n");
+}
+
+//Compound statement if, no brackets
+if(score > big)
+{
+  score++;
+  printf("You win!\n");
+}
